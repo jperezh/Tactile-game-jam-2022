3d365b688da3e61426db2b5df778b4b8952631d5
Jaime Perez <jperezh3@gmail.com>
2022-11-19 12:36:41 +0100

    Added crane actions and controll button prefab

--- Assets/Code/MapMechanics/ControlButton.cs
+++ Assets/Code/MapMechanics/ControlButton.cs
@@ -1,10 +1,32 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class ControlButton : MonoBehaviour {
+public class ControlButton : MonoBehaviour
+{
+    [SerializeField] private MagnetActions buttonAction;
+    [SerializeField] private MagnetController magnetController;
 
-    private void OnTriggerStay2D(Collider2D col)
-    {
-        Debug.Log($"[Jaime]: OnTriggerStay");
+    private void OnTriggerStay2D(Collider2D col) {
+        MoveMagnet();
+    }
+
+    private void MoveMagnet() {
+        switch (buttonAction) {
+            case MagnetActions.MoveLeft:
+                magnetController.Left();
+                break;
+            case MagnetActions.MoveRight:
+                magnetController.Right();
+                break;
+            case MagnetActions.MoveUp:
+                magnetController.Left();
+                break;
+            case MagnetActions.MoveDown:
+                magnetController.Down();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
---
+++ Assets/Code/MapMechanics/CraneAction.cs
@@ -0,0 +1,7 @@
+public enum MagnetActions
+{
+    MoveLeft,
+    MoveRight,
+    MoveUp,
+    MoveDown
+}
